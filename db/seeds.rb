01493401b8242ebd44ec903c8528b9a6284ebbf3
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-07 22:39:35 +0000

    [Add]管理者と会員、ジャンルをDBに登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ↓ データベースに登録済み
+# 管理者
+# Admin.create(email: 'sample@example.com', password: '1231025')
+# 会員
+# customer = Customer.create!(
+#   name: '愛生江尾',
+#   name_kana: 'アイウエオ',
+#   email: 'aiueo111@email.com',
+#   telephone_number: '11111111111',
+#   password: 'aaiueo',
+#   password_confirmation: 'aaiueo',
+#   is_active: true
+# )
+# ジャンル
+# genre = Genre.create(name: "肉・卵")
