f3adb8dc68e89af2fa67103ce93eac90fc9fc17d
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-08 17:40:41 +0000

    [Add]email正規化とバリデーション設定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ApplicationRecord
 
+  before_validation :normalize_email  #データが保存される前にprivateで定義したメソッドを実行
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -12,9 +14,17 @@ class Customer < ApplicationRecord
   validates :name, presence: true
   validates :name_kana, presence: true
   validates :telephone_number, uniqueness: true, presence: true #uniqueness：一意性があるか
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } #標準的なメールアドレスの形式に対し十分なバリデーションを提供
 
   def active_for_authentication? #有効会員かどうかを判断
     super && (self.is_active == true)
+  end
+
+  private
+
+  #emailの正規化処理
+  def normalize_email
+    self.email = email.downcase.strip if email.present?
   end
 
 end
