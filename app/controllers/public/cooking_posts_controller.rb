ba323474135685c9951e4349c3590462a90b12ab
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-10 22:18:32 +0000

    [Add]料理新規登録/詳細/編集ページ作成

--- app/controllers/public/cooking_posts_controller.rb
+++ app/controllers/public/cooking_posts_controller.rb
@@ -1,8 +1,21 @@
 class Public::CookingPostsController < ApplicationController
+
   def new
+    @cooking_post = CookingPost.new
   end
 
   def create
+    cooking_post = CookingPost.new(cooking_post_params)
+    cooking_post.customer_id = current_customer.id
+
+    if cooking_post.save
+      flash[:notice] = "新しい料理を投稿しました"
+      redirect_to cooking_post_path(cooking_post.id)
+    else
+      cooking_post = CookingPost.new
+      flash[:notice] = "料理の投稿ができませんでした"
+      render :new
+    end
   end
 
   def index
@@ -12,14 +25,32 @@ class Public::CookingPostsController < ApplicationController
   end
 
   def show
+    @cooking_post = CookingPost.find(params[:id])
   end
 
   def edit
+    @cooking_post = CookingPost.find(params[:id])
   end
 
   def update
+    cooking_post = CookingPost.find(params[:id])
+    if cooking_post.update(cooking_post_params)
+      flash[:notice] = "料理情報が更新されました"
+      redirect_to cooking_post_path(cooking_post.id)
+    else
+      cooking_post = CookingPost.find(params[:id])
+      flash[:notice] = "料理の更新ができませんでした"
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def cooking_post_params
+    params.require(:cooking_post).permit(:cooking_post_image, :customer_id, :name, :introduction)
+  end
+
 end
--- app/models/cooking_post.rb
+++ app/models/cooking_post.rb
@@ -12,7 +12,7 @@ class CookingPost < ApplicationRecord
   #画像の設定メソッド
   def get_cooking_post_image(width, height)
     unless cooking_post_image.attached? #投稿に画像がない場合
-      file_path = Rails.root.join('app/asets/images/o_image.jpg') #()内へのルートパス作成
+      file_path = Rails.root.join('app/assets/images/no_image.jpg') #()内へのルートパス作成
       cooking_post_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg') #imageに画像を代入
     end
     cooking_post_image.variant(resize_to_fill: [width, height]).processed #指定された幅と高さにリサイズされたデフォルト画像が代入されたcooking_post_imageを返す
