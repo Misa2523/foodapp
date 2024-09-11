5a97779bccc2099de44b4e86ad69697e93d3455d
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-11 13:54:47 +0000

    [Add]管理者側でCRUD機能の実装（管理者側の食材保存場所関連）

--- app/controllers/admin/food_strage_areas_controller.rb
+++ app/controllers/admin/food_strage_areas_controller.rb
@@ -1,19 +1,56 @@
 class Admin::FoodStrageAreasController < ApplicationController
+
   def new
+    @food_strage_area = FoodStrageArea.new
   end
 
   def create
+    @food_strage_area = FoodStrageArea.new(food_strage_area_params)
+    if @food_strage_area.save
+      flash[:notice] = "新しい食材を登録しました"
+      redirect_to admin_food_strage_areas_path
+    else
+      @food_strage_area = FoodStrageArea.new #renderでnewページを描くため、newで使う変数をこのアクション内で再定義
+      flash[:notice] = "食材の登録ができませんでした"
+      render :new
+    end
   end
 
   def index
+    @food_strage_areas = FoodStrageArea.all.page(params[:page]).per(10)
   end
 
   def destroy
+    @food_strage_area = FoodStrageArea.find(params[:id])
+    if @food_strage_area.destroy
+      flash[:notice] = "食材保存場所が削除されました"
+      redirect_to admin_food_strage_areas_path
+    else
+      flash[:notice] = "食材保存場所の削除の削除ができませんでした"
+      redirect_to admin_food_strage_areas_path
+    end
   end
 
   def edit
+    @food_strage_area = FoodStrageArea.find(params[:id])
   end
 
   def update
+    @food_strage_area = FoodStrageArea.find(params[:id])
+    if @food_strage_area.update(food_strage_area_params)
+      flash[:notice] = "食材保存場所情報が更新されました"
+      redirect_to admin_food_strage_areas_path
+    else
+      @food_strage_area = FoodStrageArea.find(params[:id]) #renderでeditページを描くため、editで使う変数をこのアクション内で再定義
+      flash[:notice] = "入力項目を正しく入力してください"
+      render :edit
+    end
   end
+
+  private
+
+  def food_strage_area_params
+    params.require(:food_strage_area).permit(:name, :place)
+  end
+
 end
