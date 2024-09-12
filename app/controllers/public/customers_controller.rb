64ace4176f6446d0e7c78c68a6765d1d3d24ed0f
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-12 21:06:06 +0000

    [Add]マイページ、マイプロフィール編集ページ作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -6,19 +6,28 @@ class Public::CustomersController < ApplicationController
 
   def posts_index
     @customer = Customer.find(params[:id])
-    @cooking_posts = @customer.cooking_posts  #会員に紐づく料理投稿を取得（idで指定された会員の投稿のみ表示するため）
-    
-    
-    @customers = Customer.all.page(params[:page]).per(10)
+    @cooking_posts = @customer.cooking_posts  #会員に紐づく料理投稿を取得（idで指定された会員の投稿のみ表示するため定義）
+
+    @customers = Customer.all.page(params[:page]).per(10) #ページネーション用に定義
   end
 
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報が更新されました"
+      redirect_to customers_my_page_path
+    else
+      flash[:notice] = "入力項目を正しく入力してください"
+      render :edit
+    end
   end
 
   def check
