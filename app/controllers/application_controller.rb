3d24fd2cfa110519357ca68c9568ac0baf410cff
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-13 23:56:44 +0000

    [Add]Aboutページにログイン別による使用可能機能の記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  #ゲストユーザーかどうか判断(ヘッダーのボタン非表示設定に使用)
-  def guest?
-    current_customer.email == "guest@example.com"
-  end
-
-  helper_method :guest? #guest?メソッドをヘルパーメソッドに指定し、ビューファイルで呼び出せるようにする
-
 end
--- app/controllers/public/cooking_posts_controller.rb
+++ app/controllers/public/cooking_posts_controller.rb
@@ -28,7 +28,7 @@ class Public::CookingPostsController < ApplicationController
 
   def show
     @cooking_post = CookingPost.find(params[:id])
-    @user = @cooking_post.customer
+    @customer = @cooking_post.customer
   end
 
   def edit
@@ -65,11 +65,11 @@ class Public::CookingPostsController < ApplicationController
     params.require(:cooking_post).permit(:cooking_post_image, :customer_id, :name, :introduction)
   end
 
-  #ログインユーザーがその投稿の投稿者か判断するメソッド（自分以外が処理を実行できないようにする）
+  #ログインユーザーがその投稿の投稿者か判断するメソッド（自分以外が処理を実行できないように、URL直打ちで遷移できないようにする）
   def is_matching_login_user
     @cooking_post = CookingPost.find(params[:id])
-    @user = @cooking_post.customer
-    unless @user.id == current_customer.id
+    @customer = @cooking_post.customer
+    unless @customer.id == current_customer.id
       redirect_to cooking_posts_path
     end
   end
