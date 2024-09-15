b031766548289937503aec63d4dac98e90a26863
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-15 19:13:35 +0000

    [Add]ヘッダー微調整

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,7 +2,7 @@ class Public::CustomersController < ApplicationController
 
   #ログイン前ユーザーによるURL直打ちでの、ページ遷移と処理を制限（全てのアクションに対し）
   before_action :authenticate_customer!
-  
+
   #application_controller.rbで定義したメソッドを実行（ゲストユーザーによるURL直打ちでの、ページ遷移と処理を制限
   before_action :restricted_guest_user, only: [:index, :posts_index, :show, :edit, :check, :update, :out]
 
