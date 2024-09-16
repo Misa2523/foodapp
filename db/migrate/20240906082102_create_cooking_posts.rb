9f5a2cbf5e07490f1257ccfad5e2973b2b1faf6d
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-16 11:39:09 +0000

    修正

--- db/migrate/20240906082102_create_cooking_posts.rb
+++ db/migrate/20240906082102_create_cooking_posts.rb
@@ -4,7 +4,7 @@ class CreateCookingPosts < ActiveRecord::Migration[6.1]
 
       t.references :customer, null: false, foreign_key: {to_table: :customers}
       t.string :name, null: false
-      t.text :introduction, null: false, index: true
+      t.text :introduction, null: false
 
       t.timestamps
     end
