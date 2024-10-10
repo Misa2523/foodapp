1d88ed77fa98301ea6881590e26bf53973caaea5
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-10-10 11:45:11 +0000

    [Add]通知機能の再実装（通知モデル編集）

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,30 @@
+# class Notification < ApplicationRecord
+
+#   #アソシエーション
+#   belongs_to :customer
+#   belongs_to :home_food
+
+#   #バリデーション設定
+#   validates :message, presence: true
+#   validates :notification_type, presence: true
+
+# end
+
+
+
+
+
+
+
+
 class Notification < ApplicationRecord
 
   #アソシエーション
   belongs_to :customer
-  belongs_to :home_food
+  belongs_to :home_food   #消す？
+
+  #ポリモーフィック関連
+  belongs_to :notifiable, polymorphic: true
 
   #バリデーション設定
   validates :message, presence: true
